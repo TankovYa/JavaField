331636e74d6acf574dad285ffd4586775fa5a486
TankovYa <tankov.ya@gmail.com>
2023-10-19 14:59:50 +0300

    create i[], found problems

--- IntegerCash/src/Main/Main.java
+++ IntegerCash/src/Main/Main.java
@@ -1,10 +1,20 @@
 package Main;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Integer[] i = {1,1,129,129};
+		System.out.print("\nArray i:");
+		for (int n=0;n<i.length;n++) {
+			System.out.print("\n\ti"+(n+1)+" = "+i[n]);
+		}
+		System.out.print("\n i1==i2:\n\t"+(i[0]==i[1]));
+		System.out.print("\n i3==i4:\n\t"+(i[2]==i[3]));
 	}
 
 }
