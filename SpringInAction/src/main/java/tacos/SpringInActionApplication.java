c0634e87fd94be437dd40c4441dc9f6bbdc2b9ce
TankovYa <tankov.ya@gmail.com>
2024-01-23 16:42:14 +0300

    глава 2

--- SpringInAction/src/main/java/tacos/SpringInActionApplication.java
+++ SpringInAction/src/main/java/tacos/SpringInActionApplication.java
@@ -2,12 +2,18 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringInActionApplication {
+public class SpringInActionApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringInActionApplication.class, args);
 	}
 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 }
