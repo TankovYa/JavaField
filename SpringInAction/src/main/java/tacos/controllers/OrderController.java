270002e4032f57c32092327c00905918bb7000ac
TankovYa <tankov.ya@gmail.com>
2024-02-07 12:39:16 +0300

    rest api вручную

--- SpringInAction/src/main/java/tacos/controllers/OrderController.java
+++ SpringInAction/src/main/java/tacos/controllers/OrderController.java
@@ -1,13 +1,21 @@
 package tacos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -49,5 +57,51 @@ public class OrderController {
 		sessionStatus.setComplete();
 		
 		return  "redirect:/";
+	}
+	
+	@PutMapping(path="/{orderId}", consumes="application/json")
+	public TacoOrder putOrder(@PathVariable("orderId") Long orderId, @RequestBody TacoOrder order) {
+		order.setId(orderId);
+		return orderRep.save(order);
+	}
+	
+	@PatchMapping(path="/{orderId}", consumes="application/json")
+	public TacoOrder patchOrder(@PathVariable("orderId") Long orderId, @RequestBody TacoOrder patch) {
+		TacoOrder order = orderRep.findById(orderId).get();
+		if (patch.getDeliveryName() != null) {
+			order.setDeliveryName(patch.getDeliveryName());
+		}
+		if (patch.getDeliveryStreet() != null) {
+			order.setDeliveryStreet(patch.getDeliveryStreet());
+		}
+		if (patch.getDeliveryCity() != null) {
+			order.setDeliveryCity(patch.getDeliveryCity());
+		}
+		if (patch.getDeliveryState() != null) {
+			order.setDeliveryState(patch.getDeliveryState());
+		}
+		if (patch.getDeliveryZip() != null) {
+			order.setDeliveryZip(patch.getDeliveryZip());
+		}
+		if (patch.getCcNumber() != null) {
+			order.setCcNumber(patch.getCcNumber());
+		}
+		if (patch.getCcExpiration() != null) {
+			order.setCcExpiration(patch.getCcExpiration());
+		}
+		if (patch.getCcCVV() != null) {
+			order.setCcCVV(patch.getCcCVV());
+		}
+		return orderRep.save(order);
+	}
+	
+	@DeleteMapping("/{orderId}")
+	public ResponseEntity<TacoOrder> deleteOrder(@PathVariable("orderId") Long orderId) {
+		try {
+			orderRep.deleteById(orderId);
+		} catch (EmptyResultDataAccessException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}	
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
--- SpringInAction/src/main/java/tacos/web/api/TacoController.java
+++ SpringInAction/src/main/java/tacos/web/api/TacoController.java
@@ -1,16 +1,25 @@
 package tacos.web.api;
 
+import java.util.Date;
 import java.util.Optional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import tacos.Taco;
@@ -40,5 +49,40 @@ public class TacoController {
 			return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
 		}
 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
+	
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Taco postTaco(@RequestBody Taco taco) {
+		return tacoRepo.save(taco);
+	}
+	
+	@PutMapping(path = "/{tacoId}", consumes = "application/json")
+	public Taco putTaco(@PathVariable("tacoId") Long tacoId, @RequestBody Taco taco) {
+		taco.setId(tacoId);
+		return tacoRepo.save(taco);
+	}
+	
+	@PatchMapping(path = "/{tacoId}", consumes = "application/json")
+	public Taco patchTaco(@PathVariable("tacoId") Long tacoId, @RequestBody Taco patch) {
+		Taco taco= tacoRepo.findById(tacoId).get();
+		if(patch.getName() != null) {
+			taco.setName(patch.getName());
+		}
+		if(patch.getIngredients() != null ) {
+			taco.setIngredients(patch.getIngredients());
+		}
+		taco.setCreatedAt(new Date());
+		return tacoRepo.save(taco);
+	}
+	
+	@DeleteMapping(path = "/{tacoId}")
+	public ResponseEntity<Taco> deleteTaco(@PathVariable("tacoId") Long tacoId) {
+		try {
+			tacoRepo.deleteById(tacoId);
+		} catch (EmptyResultDataAccessException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}	
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
